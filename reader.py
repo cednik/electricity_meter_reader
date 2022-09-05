7fa0fa95f5260e6ad1a7e0b024caeef2fda9bb5f
Kuba Streit <kuba.streit@gmail.com>
2022-09-05 23:58:27 +0200

    Add and test serial_number and software_version to/on SDM72

--- reader.py
+++ reader.py
@@ -13,6 +13,11 @@ parity = 'N'
 class MeterReader(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.registers |= {
+            "serial_number": (0xFC00, 2, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.UINT32, int, "Serial number", "", 1, 1),
+            "meter_code": (0xFC02, 1, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.INT16, int, "Meter code", "", 1, 1), # dtype should be UINT16, but it is not implemented in sdm_modbus package yet
+            "software_version": (0xFC02, 1, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.INT16, int, "Software version", "", 1, 1), # dtype should be UINT16, but it is not implemented in sdm_modbus package yet
+        }
     
     def __getattr__(self, __name: str) -> Any:
         if __name in self.registers:
@@ -20,16 +25,11 @@ class MeterReader(object):
         else:
             raise AttributeError(f'Class {self.__class__.__name__} has no attribute {__name}.')
 
-class SDM120(sdm_modbus.SDM120, MeterReader):
+class SDM120(MeterReader, sdm_modbus.SDM120):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.registers |= {
-            "serial_number": (0xFC00, 2, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.UINT32, int, "Serial number", "", 1, 1),
-            "meter_code": (0xFC02, 1, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.INT16, int, "Meter code", "", 1, 1), # dtype should be UINT16, but it is not implemented in sdm_modbus package yet
-            "software_version": (0xFC02, 1, sdm_modbus.meter.registerType.HOLDING, sdm_modbus.meter.registerDataType.INT16, int, "Software version", "", 1, 1), # dtype should be UINT16, but it is not implemented in sdm_modbus package yet
-        }
 
-class SDM72(sdm_modbus.SDM72, MeterReader):
+class SDM72(MeterReader, sdm_modbus.SDM72):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -42,13 +42,15 @@ def main(argv):
         meters = {
             'front_flat' : SDM120(parent=modbus, unit=1),
             'rear_flat'  : SDM120(parent=modbus, unit=2),
-            #'workroom'   : SDM72 (parent=modbus, unit=3)
+            'workroom'   : SDM72 (parent=modbus, unit=3)
         }
         for name, meter in meters.items():
             print(f'Meter {name }: {meter.serial_number} sw v{meter.software_version} @ {meter.unit or "broadcast"}')
         while True:
             print(f'{datetime.now():%d.%m.%Y %H:%M:%S.%f}', end='')
             for name, meter in meters.items():
+                if name == 'workroom':
+                    continue
                 try:
                     print(f'\t{name} {meter.voltage:5.1f} V, {meter.current:5.2f} A, {meter.power_active:6.1f} W, {meter.power_factor:+5.3f}, {meter.frequency:5.2f} Hz, {meter.total_energy_active:10.3f} kWh', end='')
                 except KeyboardInterrupt:
